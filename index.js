411770b7d074bc28847de83f2b360679e3a513e3
moosman.katie <moosman.katie@gmail.com>
2018-10-29 04:05:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function happyHolidaysTo (name) {
   return "Happy holidays, you!"
 }
 
-function happyHolidaysTo (holiday, name) {
-  return "Happy ${holiday}, ${name}!"
-}
+// function happyHolidaysTo (holiday, name) {
+//   return "Happy ${holiday}, ${name}!"
+// }
 
 function holidayCountdown (days, holiday) {
   return "It's ${days} until ${holiday}!"
