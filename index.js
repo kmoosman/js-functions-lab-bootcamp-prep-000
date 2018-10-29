b0595b62a11ce256b4c86f23fb070ff11fe2c554
moosman.katie <moosman.katie@gmail.com>
2018-10-29 04:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays () {
 
 function happyHolidaysTo (name) {
   var name = "you"
-  return "Happy holidays, you!"
+  return "Happy holidays, ${name}!"
 }
 
 function happyHolidayTo (holiday, name) {
